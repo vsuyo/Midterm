f168a2b7e72e4906b45cd65883deef703a7c91d2
suyovincent@gmail.com <33724032+vsuyo@users.noreply.github.com>
2018-02-06 01:26:51 +0800

    2nd commit

---
+++ addbook.php
@@ -0,0 +1,65 @@
+<?php
+require('addbooks.php');
+?>
+<html>
+
+<head>
+	<title>Book Information</title>
+    <link href="assets/css/reg.css" rel="stylesheet">
+</head>
+
+<body>
+	<form action="addbook.php" method="post">
+	<h1>Library Database</h1>
+	<fieldset>
+		<legend>Book Information</legend>
+		<label>Title:</label> <input type="text" name="title" required/><br />
+		<label>Pages:</label> <input type="number" min=1 name="pages" required/><br />
+		<label>Author:</label> <input type="text" name="author" required/><br />
+		<label>Published Year:</label> <input type="text" name="year" required/>
+        <div><br/></div>
+    <input style="float:right" type="submit" value="Add Book"  name="submit"/>
+    </fieldset>
+    <h3>List of Stored Books</h3>
+    <table border="2" align="center" cellpadding=5>
+            <thead>
+                <tr><th>Title</th>
+                    <th>Pages</th>
+                    <th>Author</th>
+                    <th>Published Year</th>
+                 
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $conn = new mysqli("localhost", "root", "", "library") or die(mysqli_error());
+                $query = $conn->query("SELECT * FROM `books` ORDER BY `book_id`") or die (mysqli_error());
+               while($fetch = $query->fetch_array()){
+                $title = $fetch['title'];
+                $pages = $fetch['pages'];
+                $author = $fetch['author'];
+                $year = $fetch['year'];
+
+
+
+                echo "<tr>
+                                                <td>$title</td>
+                                                <td>$pages</td>
+												<td>$author</td>
+												<td>$year</td>";
+               }
+                                                        ?>
+            
+                
+            </tbody>
+        </table>
+	</form>
+    <script type="text/javascript" src="assets/js/jquery-1.10.2.js"></script>
+	<script type="text/javascript">
+		function submit_form(){
+			alert("A new book has been successfully added!");
+		}
+	</script>
+
+</body>
+</html>
---
+++ addbooks.php
@@ -0,0 +1,13 @@
+   <?php
+   if(isset($_POST['submit'])){
+	$title = $_POST['title'];
+    $pages = $_POST['pages'];
+	$author = $_POST['author'];
+	$year = $_POST['year'];
+	
+    $conn = new mysqli("localhost", "root", "", "library") or die (mysqli_error());
+    $query = $conn -> query ("INSERT INTO `books`(`title`, `pages`, `author`, `year`) VALUES ('$title', '$pages', '$author', '$year')") or die (mysqli_error());
+    $conn->close();
+	echo '<script>alert("Succesfully Added!"); window.location.href="addbook.php"</script>';	
+}
+?>
