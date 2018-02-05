9fe449915e5e0fc9e52140b70ec71cfe6bd7fec0
suyovincent@gmail.com <33724032+vsuyo@users.noreply.github.com>
2018-02-06 01:30:30 +0800

    2nd commit

--- addbook.php
+++ addbook.php
@@ -13,10 +13,10 @@ require('addbooks.php');
 	<h1>Library Database</h1>
 	<fieldset>
 		<legend>Book Information</legend>
-		<label>Title:</label> <input type="text" name="title" required/><br />
-		<label>Pages:</label> <input type="number" min=1 name="pages" required/><br />
-		<label>Author:</label> <input type="text" name="author" required/><br />
-		<label>Published Year:</label> <input type="text" name="year" required/>
+		<label>Title:</label> <input type="text" name="title" placeholder = "title" required/><br />
+		<label>Pages:</label> <input type="number" min=1 name="pages" placeholder = "name" required/><br />
+		<label>Author:</label> <input type="text" name="author" placeholder = "author" required/><br />
+		<label>Published Year:</label> <input type="text" name="year" placeholder = "year" required/>
         <div><br/></div>
     <input style="float:right" type="submit" value="Add Book"  name="submit"/>
     </fieldset>
